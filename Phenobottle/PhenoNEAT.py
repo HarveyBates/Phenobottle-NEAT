d334e213a94d982331960df7234111d154f9bcad
Harvey Bates <harvey_bates@hotmail.com>
2020-06-13 11:52:12 +1000

    inital

---
+++ Phenobottle/PhenoNEAT.py
@@ -0,0 +1,12 @@
+import neat
+import time
+import os
+import random
+
+def run():
+    config = neat.config.Config(neat.DefaultGenome, n)
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    neat_config_path = os.path.join(local_dir, "NEAT-Config.txt")
+    run(neat_config_path)
