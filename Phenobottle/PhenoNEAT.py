2c0c8dbbf5e02dd0e93c4841753e4d9eb8d37cef
Harvey Bates <harvey_bates@hotmail.com>
2020-06-13 22:00:17 +1000

    commit

--- Phenobottle/PhenoNEAT.py
+++ Phenobottle/PhenoNEAT.py
@@ -1,13 +1,90 @@
-import neat
-import time
 import os
-import random
+import neat
+import datetime as dt
+import serial
+import math
+from Adafruit_MotorHAT import Adafruit_MotorHAT
 
-def run(config_path):
-    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
-    neat.DefaultSpeciesSet, neat.DefaultStagnation, "Hello")
 
-if __name__ == "__main__":
+SERIAL_PORT = '/dev/ttyACM0'
+SER = serial.Serial(SERIAL_PORT, 115200)
+MOTOR_INDEX = Adafruit_MotorHAT(addr=0x60)
+LIGHT_CONTROL = MOTOR_INDEX.getMotor(4)
+light_intensity = 5
+
+class Bottle:
+    def __init__(self):
+        self.light_intensity = []
+        self.get_optical_density()
+        self.get_OJIP()
+
+    def get_optical_density(self):
+        global optical_density
+        SER.flush()
+        SER.write(b'MeasureOpticalDensity')
+        optical_density_bytes = SER.readline(20)
+        decoded_optical_density_bytes = str(optical_density_bytes[0:len(optical_density_bytes) - 2].decode("utf-8"))
+        try:
+            data_split = [float(s) for s in decoded_optical_density_bytes.split(",")]
+            od_raw = data_split[0]
+            initial_transmittance = od_raw / INITIAL_OPTICAL_DENSITY
+            transmittance = initial_transmittance * 100
+            transmittance = round(transmittance, 2)
+            calc_optical_density = (-math.log10(initial_transmittance))
+            optical_density = round(calc_optical_density, 3)
+        except (ZeroDivisionError, ValueError) as e:
+            optical_density = 0
+        return optical_density
+    
+    def lights_on(self):
+        LIGHT_CONTROL.run(Adafruit_MotorHAT.BACKWARD)
+        LIGHT_CONTROL.setSpeed(int(light_intensity))
+
+def eval_genomes(genomes, config):
+    nets = [] # I think this is the Bottles
+    genome = 0 # This is the genomes that we are trying to optimise
+    net = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
+    nets.append(net)
+
+    genome.fitness = 0
+
+    while True:
+        Bottle.get_optical_density()
+        if od_array.length() > 0:
+            if od_array[-1] == od_array[-2]: #OD is the same
+                genome.fitness += 0.2
+            if od_array[-1] > od_array[-2]: # OD is the higher
+                genome.fitness += 1
+            if od_array[-1] < od_array[-2]: # OD is lower
+                genome.fitness -= 1
+
+    output = nets.activate((optical_density)) #takes a list or input vakyes
+    
+    if output >= 0.5:
+        light_intensity += 1
+    if output < 0: 
+        light_intensity -= 1
+
+
+def run(config_file):
+    # Load configuration.
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    pop = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+
+    # Run for up to 300 generations.
+    winner = pop.run(eval_genomes, 50)
+
+if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
-    neat_config_path = os.path.join(local_dir, "NEAT-Config.txt")
-    run(neat_config_path)
+    config_path = os.path.join(local_dir, 'NEAT-Config.txt')
+    run(config_path)
+
--- Phenobottle/PhenoSensors.py
+++ Phenobottle/PhenoSensors.py
@@ -16,44 +16,27 @@ class Sensors:
             print("Light Intensity = ", light_intensity_a)
 
     @staticmethod
-    def measure_optical_density(quiet = False):
+    def measure_optical_density():
         global od_raw, transmittance, optical_density
-        increment = 0
-        if use_sensor_data is True:
-            ser.flush()
-            root.after(1000)
-            ser.write(b'MeasureOpticalDensity')
-            optical_density_bytes = ser.readline(20)
-            decoded_optical_density_bytes = str(optical_density_bytes[0:len(optical_density_bytes) - 2].decode("utf-8"))
-            try:
-                data_split = [float(s) for s in decoded_optical_density_bytes.split(",")]
-                od_raw = data_split[0]
-                initial_transmittance = od_raw / INITIAL_OPTICAL_DENSITY
-                transmittance = initial_transmittance * 100
-                transmittance = round(transmittance, 2)
-                calc_optical_density = (-math.log10(initial_transmittance))
-                optical_density = round(calc_optical_density, 3)
-            except (ZeroDivisionError, ValueError) as e:
-                od_raw = 0
-                transmittance = 0
-                optical_density = 0
-        else:
-            try:
-                od_raw = random.randint(0, 4096)
-                initial_transmittance = od_raw / INITIAL_OPTICAL_DENSITY
-                transmittance = initial_transmittance * 100
-                transmittance = round(transmittance, 2)
-                calc_optical_density = (-math.log10(initial_transmittance))
-                optical_density = round(calc_optical_density, 3)
-            except (ZeroDivisionError, ValueError) as e:
-                od_raw = 0
-                transmittance = 0
-                optical_density = 0
+        ser.flush()
+        ser.write(b'MeasureOpticalDensity')
+        optical_density_bytes = ser.readline(20)
+        decoded_optical_density_bytes = str(optical_density_bytes[0:len(optical_density_bytes) - 2].decode("utf-8"))
+        try:
+            data_split = [float(s) for s in decoded_optical_density_bytes.split(",")]
+            od_raw = data_split[0]
+            initial_transmittance = od_raw / INITIAL_OPTICAL_DENSITY
+            transmittance = initial_transmittance * 100
+            transmittance = round(transmittance, 2)
+            calc_optical_density = (-math.log10(initial_transmittance))
+            optical_density = round(calc_optical_density, 3)
+        except (ZeroDivisionError, ValueError) as e:
+            od_raw = 0
+            transmittance = 0
+            optical_density = 0
+        return optical_density
 
-        if quiet is False:
-            print("Bit Count OD = ", od_raw)
-            print("Transmittance (%) = ", transmittance)
-            print("Optical Density = ", optical_density)
+
 
     @staticmethod
     def measure_temperature(quiet = False):
@@ -72,108 +55,26 @@ class Sensors:
             print("Temperature = ", temperature)
 
     @staticmethod
-    def measure_fluorescence(quiet = False):
-        global fo, f_300us, fj, fi, fm, variable_fluorescence, quantum_yield, vj, fm_qa, mo, performance_index, fj_fo
-        global fi_fo, fi_fj, fm_fi, fo_od, fj_od, fi_od, fm_od, variable_fluorescence_od, fm_qa_od, time_ojip, value_ojip, norm_ojip
-        if use_sensor_data is True:
-            ser.flush()
-            root.after(1000)
-            ser.write(b'MeasureFluorescence')
-        time_ojip = []
+    def measure_fluorescence():
+        ser.write(b'MeasureFluorescence')
         value_ojip = []
         norm_ojip = []
         for _ in range(2000):
-            if use_sensor_data is True:
-                fluorescence_bytes = ser.readline()
-                decoded_fluorescence_bytes = str(fluorescence_bytes[0:len(fluorescence_bytes) - 2].decode("utf-8"))
-                data_split = [float(s) for s in decoded_fluorescence_bytes.split("\t")]
-                x_data = data_split[0]
-                y_data = float(data_split[1])
-            else:
-                x_data = (random.randint(0, 100) / 10)
-                y_data = float((random.randint(0, 100) / 10))
-
-            y_data = ((y_data * ARDUINO_FLUORESCENCE_REFERENCE) / 4096)
-            time_ojip.append(x_data)
+            fluorescence_bytes = ser.readline()
+            decoded_fluorescence_bytes = str(fluorescence_bytes[0:len(fluorescence_bytes) - 2].decode("utf-8"))
+            data_split = [float(s) for s in decoded_fluorescence_bytes.split("\t")]
+            y_data = float(data_split[1])
+            y_data = ((y_data * 3.3) / 4096)
             value_ojip.append(round(y_data, 3))
         fo = value_ojip[4]
-        f_300us = value_ojip[36]
-        fj = value_ojip[248]
-        fi = value_ojip[1023]
-        fm = max(value_ojip[10:]) # Ignores the first few values as there can be a peak at 1us due to the amplifier
-        fm_fi = (fm - fi)
-        fi_fj = (fi - fj)
-        fj_fo = (fj - fo)
-        fm_fj = (fm - fj)
-        fi_fo = (fi - fo)
-        variable_fluorescence = (fm - fo)
-
-        try:
-            vj = (fj - fo) / variable_fluorescence
-            vj = round(vj, 2)
-        except ZeroDivisionError:
-            vj = 0
-
-        try:
-            quantum_yield = variable_fluorescence / fm
-            quantum_yield = round(quantum_yield, 2)
-        except ZeroDivisionError:
-            quantum_yield = 0
-
-        try:
-            mo = 4 * ((f_300us - fo) / variable_fluorescence)
-            mo = round(mo, 3)
-        except ZeroDivisionError:
-            mo = 0
-
-        try:
-            performace_index = ((1 - (fo / fm)) / (mo / vj)) * (variable_fluorescence / fo) * ((1 - vj) / vj)
-            performace_index = round(performace_index, 3)
-        except ZeroDivisionError:
-            performace_index = 0
-
-        try:
-            fm_qa = (fj - fo) / fj
-            fm_qa = round(fm_qa, 3)
-        except ZeroDivisionError:
-            fm_qa = 0
-
+        fm = max(value_ojip[10:]) 
         try:
             for e in range(len(value_ojip)):
                 rvf = ((value_ojip[e] - fo) / (fm - fo))
                 norm_ojip.append(round(rvf, 3))
         except ZeroDivisionError:
             norm_ojip = 0
-
-        try:
-            fo_od = round((fo / optical_density), 3)
-        except ZeroDivisionError:
-            fo_od = 0
-
-        try:
-            fj_od = round((fj / optical_density), 3)
-        except ZeroDivisionError:
-            fj_od = 0
-
-        try:
-            fi_od = round((fi / optical_density), 3)
-        except ZeroDivisionError:
-            fi_od = 0
-
-        try:
-            fm_od = round((fm / optical_density), 3)
-        except ZeroDivisionError:
-            fm_od = 0
-
-        try:
-            variable_fluorescence_od = round((variable_fluorescence / optical_density), 3)
-        except:
-            variable_fluorescence_od = 0
-
-        try:
-            fm_qa_od = round((fm_qa / optical_density), 3)
-        except ZeroDivisionError:
-            fm_qa_od = 0
+        return norm_ojip
 
 
 class Test:
